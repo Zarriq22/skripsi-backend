93dacacd5b60e66e01dd3cf22a5d839e3c701ab5
Zarriq22 <107358902+Zarriq22@users.noreply.github.com>
2025-05-20 14:07:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const chatRoutes = require('./routes/chatRoutes');
 const app = express();
 const PORT = 5000;
 
-const allowedOrigins = ['https://skripsi-frontend-sigma.vercel.app'];
-
 // koneksi database
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -24,8 +22,21 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error('❌ Gagal konek MongoDB:', err));
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://skripsi-frontend-sigma.vercel.app',
+];
+
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://your-frontend-url.vercel.app'],
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps, curl, postman)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }));
 app.use(express.json());
